0baaf62ba28ab38dc8ae6e2e7f2c547903546b20
RODRIGO LODEIRO <fachastorm@MacBook-Pro.local>
2019-04-05 15:33:12 -0400

    added gameplay functions

--- blackjack.py
+++ blackjack.py
@@ -70,3 +70,58 @@ class Chips():
     
     def lose_bet(self):
         self.total -= self.bet
+
+
+def take_bet(chips):
+    
+    while True: 
+
+        try: 
+            chips.bet = int(input("How many chips would you like to bet?"))
+        except: 
+            print("Sorry please provide an integer")
+        else: 
+            if chips.bet > chips.total: 
+                print(f"Sorry, you do not have enough chips! You have: {chips.total}")
+            else:
+                break
+
+def hit(deck, hand):
+
+    hand.add_card(deck.deal())
+    hand.adjust_for_aces()
+
+def hit_or_stand(deck, hand):
+    global playing
+
+    while True: 
+        x = input('Hit or Stand? Enter h or s')
+        
+        if x[0].lower() == 'h':
+            hit(deck, hand)
+        
+        elif x[0].lower() == 's':
+            print("Player Stands, Dealer's Turn")
+            playing = False
+        else:
+            print("Sorry, I did not understand that. Please enter h or s only!")
+            continue
+
+def player_busts(player, dealer, chips):
+    print('Player busts!')
+    chips.lose_bet()
+
+def player_wins(player, dealer, chips):
+    print('Player wins!')
+    chips.win_bet()
+
+def dealer_busts(player, dealer, chips):
+    print('Player wins! Dealer Busts!')
+    chips.win_bet()
+
+def dealer_wins(player, dealer, chips):
+    print('Dealer Wins!')
+    chips.lose_bet()
+    
+def push(player, dealer, chips):
+    print('Push')
